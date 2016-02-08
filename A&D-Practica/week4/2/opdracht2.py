443879f7a669c94bbeb7ed94502fccbcaa509556
koen de gruijter <koen.degruijter@student.hu.nl>
2016-02-08 12:40:37 +0100

    added func for koen

--- A&D-Practica/week4/2/opdracht2.py
+++ A&D-Practica/week4/2/opdracht2.py
@@ -1,15 +1,21 @@
 import random
 
-num = random.random()
-hashNum= hash( num )
-dict = {hashNum: num}
-
-while True:
+def sameHash():
     num = random.random()
-    hashNum = hash( num )
-    if hashNum in dict:
-        print('hash(', dict.get( hashNum ), ') == hash(', num, ') == ', hashNum )
-        break
-    else:
-        print('loop')
-        dict[hashNum] = num
+    hashNum= hash( num )
+    dict = {hashNum: num}
+
+    while True:
+        num = random.random()
+        hashNum = hash( num )
+        if hashNum in dict:
+            print('hash(', dict.get( hashNum ), ') == hash(', num, ') == ', hashNum )
+            return {dict.get( hashNum ),num}
+            break
+        else:
+            dict[hashNum] = num
+            if len(dict)>1000000:
+                print('te lang')
+                break;
+
+print(sameHash())
