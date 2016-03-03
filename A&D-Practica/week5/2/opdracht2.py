1eec8beb193ecb74c7eff1808d11489a89e37126
Koen van der Kruk <koenvdkruk@gmail.com>
2016-03-03 12:15:21 +0100

    Added week2

---
+++ A&D-Practica/week5/2/opdracht2.py
@@ -0,0 +1,78 @@
+import queue
+INFINITY = float("inf")
+class myqueue( list):
+
+    def enqueue(self,s):
+        self.append(s)
+
+    def dequeue(self):
+        return self.pop(0)
+
+
+class Vertex:
+    def __init__(self, data):
+        self.data = data
+
+    def __repr__(self): # voor afdrukken
+        return str(self.data)
+
+    def __lt__(self, other): # voor sorteren
+        return self.data < other.data
+def vertices(G):
+    a = list(G.keys())
+    a.sort()
+    return a
+
+def edges(G):
+    a = []
+    for u in vertices(G):
+        for v in G[u]:
+            a.append((u,v))
+    return a
+
+v = [Vertex(i) for i in range(5)]
+G = {v[0]:[v[1]],v[1]:[v[0],v[2],v[3],v[4]],v[2]:[v[1],v[3]],v[3]:[v[1],v[2]],v[4]:[]}
+
+print("vertices(G):",vertices(G))
+print("edges(G):",edges(G))
+
+def show_tree_info(G):
+    print('tree:', end = ' ')
+    for v in vertices(G):
+        print('(' + str(v), end = '')
+        if hasattr(v,'distance'):
+            print(',d:' + str(v.distance), end = '')
+        if hasattr(v,'predecessor'):
+            print(',p:' + str(v.predecessor), end = '')
+        print(')', end = ' ')
+    print()
+
+show_tree_info(G)
+
+def no_cycles(G):
+
+    V = vertices(G)
+    s=V[0]
+    s.predecessor = None # s krijgt het attribuut 'predecessor'
+    s.distance = 0 # s krijgt het attribuut 'distance'
+    for v in V:
+        if v != s:
+            v.distance = INFINITY # v krijgt het attribuut 'distance'
+    q = myqueue()
+    q.enqueue(s) # plaats de startnode in de queue
+    while q:
+        u = q.dequeue()
+        for v in G[u]:
+            if v.distance == INFINITY: # v is nog niet bezocht
+                v.distance = u.distance + 1
+                v.predecessor = u # v krijgt het attribuut 'predecessor'
+                q.enqueue(v) # plaats de buren van v in de queue
+            else:
+                if v.predecessor != u:
+                    
+    for dummy in V:
+        if dummy.distance== INFINITY:
+            return False
+    return True
+
+print(is_connected(G))
