298507324a4c6ea08a4a1b26b0d253fa2571a3a5
Koen van der Kruk <koenvdkruk@gmail.com>
2016-03-14 14:56:52 +0100

    Changed faculty course

--- A&D-Practica/week4/3/opdracht3.py
+++ A&D-Practica/week4/3/opdracht3.py
@@ -1,15 +1,16 @@
 def fac(n):
-     if n == 1:
-          return 1;
-     else:
-          return n * fac(n - 1)
+     totalValue = 1
+     for number in range(n, 1, -1):
+        totalValue *= number
+     return totalValue
 
 def B(n,k):
-     return((fac(n)//fac(k))//fac(n-k))
+    cFac = fac(n - k)
+    nFac = fac(n)
+    kFac = fac(k)
+    return nFac // kFac // cFac
+
 
 print("Faculteit van 9: " + str(fac(9)))
 print("Resultaat van B(100,50): " + str(B(100,50)))
-
-
-
 
